f1de620526514857e3387057419546675c725e92
David Egan <david@zenrocklabs.io>
2024-11-04 09:00:26 +0000

    fix solana parse tx test

--- x/treasury/types/wallet_solana_test.go
+++ x/treasury/types/wallet_solana_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"crypto/ed25519"
 	"crypto/sha256"
-	"fmt"
+	"encoding/hex"
 	"log"
 	"testing"
 
@@ -45,56 +45,58 @@ func TestSolanaWalletAddress(t *testing.T) {
 // TestSolanaParseTx
 func TestSolanaParseTx(t *testing.T) {
 	testCases := []struct {
-		name    string
-		to      string
-		amount  int64
-		txBytes string
+		name        string
+		to          string
+		amount      int64
+		txHexstring string
 	}{
 		{
-			name:    "Test Case 0",
-			to:      "FYJ5gRsqYAwWcxL7LsxsZuunWTysMF7if9Sp5nPADQYF",
-			amount:  10_042,
-			txBytes: "0x01000103e01763c7d59132d8423e17a8e285d86d0ae555bae8ce098ae14b5fbc709bb238d807ef6c24d679daf65e8097a455a26dae192524f343b27d1c998a9407c05cc00000000000000000000000000000000000000000000000000000000000000000f0857f2a581eea6588796d8d39e204dd354ea2d8bad46e9fc7591bc5f00c534d01020200010c020000003a27000000000000",
+			name:        "Test Case 0",
+			to:          "FYJ5gRsqYAwWcxL7LsxsZuunWTysMF7if9Sp5nPADQYF",
+			amount:      10_042,
+			txHexstring: "0x01000103e01763c7d59132d8423e17a8e285d86d0ae555bae8ce098ae14b5fbc709bb238d807ef6c24d679daf65e8097a455a26dae192524f343b27d1c998a9407c05cc00000000000000000000000000000000000000000000000000000000000000000f0857f2a581eea6588796d8d39e204dd354ea2d8bad46e9fc7591bc5f00c534d01020200010c020000003a27000000000000",
 		},
 		{
-			name:    "Test Case 1",
-			to:      "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
-			amount:  1_000_013,
-			txBytes: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a0000000000000000000000000000000000000000000000000000000000000000a1e2e5e4e30c35c282579e0338eeb9739f34f06d3db52e024e8767fcdb4dd47501020200010c020000004d420f0000000000",
+			name:        "Test Case 1",
+			to:          "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
+			amount:      1_000_013,
+			txHexstring: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a0000000000000000000000000000000000000000000000000000000000000000a1e2e5e4e30c35c282579e0338eeb9739f34f06d3db52e024e8767fcdb4dd47501020200010c020000004d420f0000000000",
 		},
 		{
-			name:    "Test Case 2",
-			to:      "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
-			amount:  1_000_000_042,
-			txBytes: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a0000000000000000000000000000000000000000000000000000000000000000bd349395c46ca6be0be09c0989649ecb2def6488a15bd6bdf44d36f9ca5aabaa01020200010c020000002aca9a3b00000000",
+			name:        "Test Case 2",
+			to:          "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
+			amount:      1_000_000_042,
+			txHexstring: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a0000000000000000000000000000000000000000000000000000000000000000bd349395c46ca6be0be09c0989649ecb2def6488a15bd6bdf44d36f9ca5aabaa01020200010c020000002aca9a3b00000000",
 		},
 		{
-			name:    "Test Case 3",
-			to:      "5Wvu3L3vVkQi2RPA12sTbzFTPgzgim5kQc3iRHFVw6zZ",
-			amount:  10_042,
-			txBytes: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d3743198871268c38c8e04a2aaa78618bf49d085c39c2631e37d3321d10c8b8c0ae0000000000000000000000000000000000000000000000000000000000000000b25330dd9a65d49b61bb4993123e10f813b0f8a2740d7a2043a246d866ade95901020200010c020000003a27000000000000",
+			name:        "Test Case 3",
+			to:          "5Wvu3L3vVkQi2RPA12sTbzFTPgzgim5kQc3iRHFVw6zZ",
+			amount:      10_042,
+			txHexstring: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d3743198871268c38c8e04a2aaa78618bf49d085c39c2631e37d3321d10c8b8c0ae0000000000000000000000000000000000000000000000000000000000000000b25330dd9a65d49b61bb4993123e10f813b0f8a2740d7a2043a246d866ade95901020200010c020000003a27000000000000",
 		},
 		{
-			name:    "Debug JS app",
-			to:      "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
-			amount:  10_042,
-			txBytes: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a0000000000000000000000000000000000000000000000000000000000000000530441528372542b2f23866d8cc58a6b8d764aee09f9c26fb263513e7fed3d8d01020200010c020000003a27000000000000",
+			name:        "Debug JS app",
+			to:          "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
+			amount:      10_042,
+			txHexstring: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a0000000000000000000000000000000000000000000000000000000000000000530441528372542b2f23866d8cc58a6b8d764aee09f9c26fb263513e7fed3d8d01020200010c020000003a27000000000000",
 		},
 		{
-			name:    "Debug JS app - FINAL",
-			to:      "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
-			amount:  10_042,
-			txBytes: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a00000000000000000000000000000000000000000000000000000000000000006bda0ef1adffccbcf2f2425b1af761b9db4122dce6a7889e4ddb2520baff79e801020200010c020000003a27000000000000",
+			name:        "Debug JS app - FINAL",
+			to:          "HpzusjfWgokpwuz6D8GhyCELJM83e6FC7KeAvzbXtF6R",
+			amount:      10_042,
+			txHexstring: "0x01000103ec683a77d4e09795bd0ee0828a553fc7acdd3c8d537f95945579b6780c429d37fa06c50dc073d2007afead92cfa489ee7ebe9fcf44253515b3301040938dba0a00000000000000000000000000000000000000000000000000000000000000006bda0ef1adffccbcf2f2425b1af761b9db4122dce6a7889e4ddb2520baff79e801020200010c020000003a27000000000000",
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			wallet := solanaWallet(t, "example seed")
-			transfer, err := wallet.ParseTx(hexutil.MustDecode(tc.txBytes), nil)
+			txBytes := hexutil.MustDecode(tc.txHexstring)
+			transfer, err := wallet.ParseTx(txBytes, nil)
+			expectedDataForSigning := []byte(hex.EncodeToString(hexutil.MustDecode(tc.txHexstring)))
 			require.NoError(t, err)
 			require.Equal(t, []byte(tc.to), transfer.To, "to address mismatch")
 			require.Equal(t, tc.amount, transfer.Amount.Int64(), "amount mismatch")
-			require.Equal(t, tc.txBytes, fmt.Sprintf("%#x", transfer.DataForSigning), "data for signing mismatch")
+			require.Equal(t, hex.EncodeToString(expectedDataForSigning), hex.EncodeToString(transfer.DataForSigning), "data for signing mismatch")
 		})
 	}
 }
