7c5016f649c5b4aed0bc694cfeb09c8ebdff71ec
Martin <martin@zenrocklabs.io>
2024-11-13 11:50:19 +0100

    feat: added mint module and updated imports

---
+++ x/mint/testutil/app_config.go
@@ -0,0 +1,24 @@
+package testutil
+
+import (
+	_ "github.com/Zenrock-Foundation/zrchain/v5/x/mint" // import as blank for app wiring
+	"github.com/cosmos/cosmos-sdk/testutil/configurator"
+	_ "github.com/cosmos/cosmos-sdk/x/auth"           // import as blank for app wiring
+	_ "github.com/cosmos/cosmos-sdk/x/auth/tx/config" // import as blank for app wiring
+	_ "github.com/cosmos/cosmos-sdk/x/bank"           // import as blank for app wiring
+	_ "github.com/cosmos/cosmos-sdk/x/consensus"      // import as blank for app wiring
+	_ "github.com/cosmos/cosmos-sdk/x/genutil"        // import as blank for app wiring
+	_ "github.com/cosmos/cosmos-sdk/x/params"         // import as blank for app wiring
+	_ "github.com/cosmos/cosmos-sdk/x/staking"        // import as blank for app wiring
+)
+
+var AppConfig = configurator.NewAppConfig(
+	configurator.AuthModule(),
+	configurator.BankModule(),
+	configurator.StakingModule(),
+	configurator.TxModule(),
+	configurator.ConsensusModule(),
+	configurator.ParamsModule(),
+	configurator.GenutilModule(),
+	configurator.MintModule(),
+)
