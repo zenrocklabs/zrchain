78decbbc11e81c17305328a1ed5aef7b171f8eea
Sasha Duke <sasha@zenrocklabs.io>
2024-11-15 14:52:14 +0000

    fix: sidecar + add temp logging

--- sidecar/eth_oracle.go
+++ sidecar/eth_oracle.go
@@ -71,6 +71,7 @@ func (o *Oracle) fetchAndProcessState(contractInstance *middleware.ContractZRSer
 	if err != nil {
 		return fmt.Errorf("failed to get contract state: %w", err)
 	}
+	log.Printf("Delegations: %v\n", delegations)
 
 	header, err := o.EthClient.HeaderByNumber(ctx, targetBlockNumber)
 	if err != nil {
