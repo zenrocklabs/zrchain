fbb79d536c59bb28f6c8a813c5eb1974178b68df
Martin <martin@zenrocklabs.io>
2024-11-18 09:43:13 +0100

    fix: splitkeyringfee test

--- x/treasury/keeper/keeper_test.go
+++ x/treasury/keeper/keeper_test.go
@@ -7,54 +7,53 @@ import (
 	"cosmossdk.io/log"
 	"cosmossdk.io/store"
 	"cosmossdk.io/store/metrics"
+	"github.com/Zenrock-Foundation/zrchain/v5/app/params"
 	keepertest "github.com/Zenrock-Foundation/zrchain/v5/testutil/keeper"
 	"github.com/Zenrock-Foundation/zrchain/v5/testutil/sample"
 	treasuryModule "github.com/Zenrock-Foundation/zrchain/v5/x/treasury/module"
 	"github.com/Zenrock-Foundation/zrchain/v5/x/treasury/types"
 	dbm "github.com/cosmos/cosmos-db"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 type bankKeeperMock struct {
-	SendCoinsCalled                    int
-	SendCoinsFromAccountToModuleCalled int
-
-	FromAddrs []sdk.AccAddress
-	ToAddrs   []sdk.AccAddress
-	Amounts   []sdk.Coins
-	Modules   []string
+	transactions []struct {
+		fromAddr sdk.AccAddress
+		toAddr   sdk.AccAddress
+		toModule string
+		amount   sdk.Coins
+	}
 }
 
 func newBankKeeperMock() *bankKeeperMock {
 	return &bankKeeperMock{
-		SendCoinsCalled:                    0,
-		SendCoinsFromAccountToModuleCalled: 0,
-		FromAddrs:                          []sdk.AccAddress{},
-		ToAddrs:                            []sdk.AccAddress{},
-		Amounts:                            []sdk.Coins{},
-		Modules:                            []string{},
+		transactions: make([]struct {
+			fromAddr sdk.AccAddress
+			toAddr   sdk.AccAddress
+			toModule string
+			amount   sdk.Coins
+		}, 0),
 	}
 }
 
-func (m *bankKeeperMock) SendCoins(_ context.Context, fromAddr, toAddr sdk.AccAddress, amt sdk.Coins) error {
-	m.SendCoinsCalled++
-
-	m.FromAddrs = append(m.FromAddrs, fromAddr)
-	m.ToAddrs = append(m.ToAddrs, toAddr)
-	m.Amounts = append(m.Amounts, amt)
-
+func (b *bankKeeperMock) SendCoins(ctx context.Context, fromAddr, toAddr sdk.AccAddress, amt sdk.Coins) error {
+	b.transactions = append(b.transactions, struct {
+		fromAddr sdk.AccAddress
+		toAddr   sdk.AccAddress
+		toModule string
+		amount   sdk.Coins
+	}{fromAddr, toAddr, "", amt})
 	return nil
 }
 
-func (m *bankKeeperMock) SendCoinsFromAccountToModule(_ context.Context, fromAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
-	m.SendCoinsFromAccountToModuleCalled++
-
-	m.FromAddrs = append(m.FromAddrs, fromAddr)
-	m.Modules = append(m.Modules, recipientModule)
-	m.Amounts = append(m.Amounts, amt)
-
+func (b *bankKeeperMock) SendCoinsFromAccountToModule(ctx context.Context, fromAddr sdk.AccAddress, toModule string, amt sdk.Coins) error {
+	b.transactions = append(b.transactions, struct {
+		fromAddr sdk.AccAddress
+		toAddr   sdk.AccAddress
+		toModule string
+		amount   sdk.Coins
+	}{fromAddr, sdk.AccAddress{}, toModule, amt})
 	return nil
 }
 
@@ -68,6 +67,11 @@ func Test_TreasuryKeeper_splitKeyringFee(t *testing.T) {
 		fee        uint64
 		commission uint64
 	}
+
+	// Generate addresses once and reuse them
+	addrFrom := sample.AccAddress()
+	addrTo := sample.AccAddress()
+
 	tests := []struct {
 		name    string
 		args    args
@@ -78,13 +82,78 @@ func Test_TreasuryKeeper_splitKeyringFee(t *testing.T) {
 			name:    "PASS: Send fee to address",
 			wantErr: false,
 			args: args{
-				feeAddr:    sample.AccAddress(),
+				feeAddr:    addrTo,
 				fee:        1000,
 				commission: 10,
 			},
 			want: want{
 				fee:        900,
 				commission: 100,
+			},
+		},
+		{
+			name:    "PASS: Send fee to module",
+			wantErr: false,
+			args: args{
+				feeAddr:    types.KeyringCollectorName,
+				fee:        1000,
+				commission: 10,
+			},
+			want: want{
+				fee:        900,
+				commission: 100,
+			},
+		},
+		{
+			name:    "PASS: Zero commission",
+			wantErr: false,
+			args: args{
+				feeAddr:    addrTo,
+				fee:        1000,
+				commission: 0,
+			},
+			want: want{
+				fee:        1000,
+				commission: 0,
+			},
+		},
+		{
+			name:    "PASS: 100% commission",
+			wantErr: false,
+			args: args{
+				feeAddr:    addrTo,
+				fee:        1000,
+				commission: 100,
+			},
+			want: want{
+				fee:        0,
+				commission: 1000,
+			},
+		},
+		{
+			name:    "PASS: Small fee amount",
+			wantErr: false,
+			args: args{
+				feeAddr:    addrTo,
+				fee:        10,
+				commission: 10,
+			},
+			want: want{
+				fee:        9,
+				commission: 1,
+			},
+		},
+		{
+			name:    "FAIL: Commission is greater than 100",
+			wantErr: true,
+			args: args{
+				feeAddr:    addrTo,
+				fee:        10,
+				commission: 101,
+			},
+			want: want{
+				fee:        0,
+				commission: 0,
 			},
 		},
 	}
@@ -103,26 +172,60 @@ func Test_TreasuryKeeper_splitKeyringFee(t *testing.T) {
 				Params: types.DefaultParams(),
 			}
 
-			addrFrom := sample.AccAddress()
-			addrTo := sample.AccAddress()
+			if tt.args.commission > 100 {
+				err := treasuryKeeper.SplitKeyringFee(ctx, addrFrom, tt.args.feeAddr, tt.args.fee)
+				require.Error(t, err)
+				return
+			}
 
 			tkGenesis.Params.KeyringCommission = tt.args.commission
 			tkGenesis.Params.KeyringCommissionDestination = tt.args.feeAddr
 
 			treasuryModule.InitGenesis(ctx, treasuryKeeper, tkGenesis)
 
-			err := treasuryKeeper.SplitKeyringFee(ctx, addrFrom, addrTo, 1000)
-			require.Nil(t, err)
+			err := treasuryKeeper.SplitKeyringFee(ctx, addrFrom, tt.args.feeAddr, tt.args.fee)
+			require.NoError(t, err)
 
-			assert.Equal(t, 2, bkmock.SendCoinsCalled)
-			assert.Equal(t, sdk.MustAccAddressFromBech32(tt.args.feeAddr), bkmock.ToAddrs[0])
-			assert.Equal(t, sdk.MustAccAddressFromBech32(addrTo), bkmock.ToAddrs[1])
+			// Verify transactions
+			require.Equal(t, 2, len(bkmock.transactions), "Expected exactly two transactions")
 
-			assert.Equal(t, sdk.MustAccAddressFromBech32(addrFrom), bkmock.FromAddrs[0])
-			assert.Equal(t, sdk.MustAccAddressFromBech32(addrFrom), bkmock.FromAddrs[1])
+			// First transaction (commission)
+			require.Equal(t,
+				addrFrom,
+				sdk.MustBech32ifyAddressBytes(sdk.Bech32MainPrefix, bkmock.transactions[0].fromAddr),
+				"First transaction from address should be sender",
+			)
+			require.Equal(t,
+				types.KeyringCollectorName,
+				bkmock.transactions[0].toModule,
+				"First transaction should go to KeyringCollector module",
+			)
+			require.Equal(t,
+				tt.want.commission,
+				bkmock.transactions[0].amount.AmountOf(params.BondDenom).Uint64(),
+				"First transaction amount should be commission",
+			)
 
-			assert.Equal(t, tt.want.commission, bkmock.Amounts[0].AmountOf("urock").Uint64())
-			assert.Equal(t, tt.want.fee, bkmock.Amounts[1].AmountOf("urock").Uint64())
+			// Second transaction
+			require.Equal(t,
+				addrFrom,
+				sdk.MustBech32ifyAddressBytes(sdk.Bech32MainPrefix, bkmock.transactions[1].fromAddr),
+				"Second transaction from address should be sender",
+			)
+			if tt.args.feeAddr == types.KeyringCollectorName {
+				require.Equal(t,
+					types.KeyringCollectorName,
+					bkmock.transactions[1].toModule,
+					"Second transaction should go to KeyringCollector module",
+				)
+			} else {
+				require.Equal(t,
+					tt.args.feeAddr,
+					sdk.MustBech32ifyAddressBytes(sdk.Bech32MainPrefix, bkmock.transactions[1].toAddr),
+					"Second transaction should go to recipient",
+				)
+			}
+			require.Equal(t, tt.want.fee, bkmock.transactions[1].amount.AmountOf(params.BondDenom).Uint64(), "Second transaction amount should be remaining fee")
 		})
 	}
 }
