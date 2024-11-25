ee3655a0422829c7be6489f9e4aecc2d7b4ca044
Martin <martin@zenrocklabs.io>
2024-11-25 14:32:18 +0100

    fix: upgrades.go

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,14 +1,10 @@
 package app
 
 import (
-	"context"
-
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades"
 	v6 "github.com/Zenrock-Foundation/zrchain/v5/app/upgrades/v6"
-	v3 "github.com/Zenrock-Foundation/zrchain/v5/x/mint/migrations/v3"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 var Upgrades = []upgrades.Upgrade{
@@ -27,15 +23,15 @@ func (app ZenrockApp) RegisterUpgradeHandlers() {
 			upgrade.CreateUpgradeHandler(app.ModuleManager, app.Configurator()),
 		)
 
-		ctx := context.Background()
+		// ctx := context.Background()
 
-		mintAccBase := authtypes.NewEmptyModuleAccount(v3.ModuleName, authtypes.Minter, authtypes.Burner)
+		// mintAccBase := authtypes.NewEmptyModuleAccount(v3.ModuleName, authtypes.Minter, authtypes.Burner)
 
-		// Update the permissions
-		macc := authtypes.NewModuleAccount(mintAccBase.BaseAccount, authtypes.Minter, authtypes.Burner)
+		// // Update the permissions
+		// macc := authtypes.NewModuleAccount(mintAccBase.BaseAccount, authtypes.Minter, authtypes.Burner)
 
-		// Save the updated account
-		app.AccountKeeper.SetModuleAccount(ctx, macc)
+		// // Save the updated account
+		// app.AccountKeeper.SetModuleAccount(ctx, macc)
 
 		if upgradeInfo.Name == upgrade.UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 			// configure store loader that checks if version == upgradeHeight and applies store upgrades
