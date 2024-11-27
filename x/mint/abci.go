acd37294ab9c37869d1f75b1dd25de7024964ae5
Ognyan Nikolov <ognyan@zenrocklabs.io>
2024-11-27 15:05:01 +0200

    send keyring creation fees to the keyring collector

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -14,9 +14,6 @@ import (
 func BeginBlocker(ctx context.Context, k keeper.Keeper, ic types.InflationCalculationFn) error {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, telemetry.Now(), telemetry.MetricKeyBeginBlocker)
 
-	perms := k.GetModuleAccountPerms(ctx)
-	fmt.Println("Mint Module Permissions at BeginBlocker:", perms)
-
 	// fetch stored minter & params
 	minter, err := k.Minter.Get(ctx)
 	if err != nil {
@@ -27,13 +24,6 @@ func BeginBlocker(ctx context.Context, k keeper.Keeper, ic types.InflationCalcul
 	if err != nil {
 		return err
 	}
-
-	// TODO remove
-	fmt.Println("---")
-
-	// TODO - create tests
-	// TODO - adjust events
-	// TODO - remove legacy minting mechanism
 
 	// recalculate inflation rate
 	totalStakingSupply, err := k.StakingTokenSupply(ctx)
@@ -51,40 +41,25 @@ func BeginBlocker(ctx context.Context, k keeper.Keeper, ic types.InflationCalcul
 		return err
 	}
 
-	// TODO - remove
-	fmt.Printf("mint module balance:\t\t%v\n", mintModuleBalance)
-
 	totalRewards, err := k.ClaimTotalRewards(ctx)
 	if err != nil {
 		return err
 	}
-
-	// TODO - remove
-	fmt.Printf("total rewards:\t\t\t%v\n", totalRewards)
 
 	totalRewardsRest, err := k.BaseDistribution(ctx, totalRewards)
 	if err != nil {
 		return err
 	}
 
-	// TODO - remove
-	fmt.Printf("rewards after burn&pw:\t\t%v\n", totalRewardsRest)
-
 	totalBondedTokens, err := k.TotalBondedTokens(ctx)
 	if err != nil {
 		return err
 	}
 
-	// TODO - remove
-	fmt.Printf("total bonded tokens:\t\t%v\n", totalBondedTokens)
-
 	totalBlockStakingReward, err := k.NextStakingReward(ctx, totalBondedTokens)
 	if err != nil {
 		return err
 	}
-
-	// TODO - remove
-	fmt.Printf("total staking rewards:\t\t%v\n", totalBlockStakingReward)
 
 	if totalBlockStakingReward.Amount.GT(totalRewardsRest.Amount) {
 		topUpAmount, err := k.CalculateTopUp(ctx, totalBlockStakingReward, totalRewardsRest)
