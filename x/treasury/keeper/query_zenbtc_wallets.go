0e477f834ab52636a3da5f685faae18d43c01170
Martin <martin@zenrocklabs.io>
2024-12-09 14:25:10 +0100

    feat: zenbtc wallets query

---
+++ x/treasury/keeper/query_zenbtc_wallets.go
@@ -0,0 +1,73 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/Zenrock-Foundation/zrchain/v5/x/treasury/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) ZenbtcWallets(
+	goCtx context.Context,
+	req *types.QueryZenbtcWalletsRequest,
+) (*types.QueryZenbtcWalletsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	keys, pageRes, err := query.CollectionFilteredPaginate(
+		goCtx,
+		k.KeyStore,
+		req.Pagination,
+		func(key uint64, value types.Key) (bool, error) {
+
+			if value.ZenbtcMetadata == nil {
+				return false, nil
+			}
+
+			switch {
+			case req.ChainId != 0 && value.ZenbtcMetadata.ChainId != req.ChainId:
+				return false, nil
+			case req.ChainType != types.WalletType_WALLET_TYPE_UNSPECIFIED && value.ZenbtcMetadata.ChainType != req.ChainType:
+				return false, nil
+			case req.RecipientAddr != "" && value.ZenbtcMetadata.RecipientAddr != req.RecipientAddr:
+				return false, nil
+			case req.ReturnAddr != "" && value.ZenbtcMetadata.ReturnAddress != req.ReturnAddr:
+				return false, nil
+			}
+
+			keyType := types.KeyType_KEY_TYPE_UNSPECIFIED
+
+			recipientAddressMatch := (req.RecipientAddr == "" || value.ZenbtcMetadata.RecipientAddr == req.RecipientAddr)
+			chainIdMatch := (req.ChainId == 0 || value.ZenbtcMetadata.ChainId == req.ChainId)
+			returnAddrMatch := (req.ReturnAddr == "" || value.ZenbtcMetadata.ReturnAddress == req.ReturnAddr)
+			keyTypeMatch := (value.Type == keyType || keyType == types.KeyType_KEY_TYPE_UNSPECIFIED)
+
+			return recipientAddressMatch && keyTypeMatch && chainIdMatch && returnAddrMatch, nil
+		},
+		func(key uint64, value types.Key) (*types.KeyAndWalletResponse, error) {
+			return &types.KeyAndWalletResponse{
+				Key: &types.KeyResponse{
+					Id:             value.Id,
+					WorkspaceAddr:  value.WorkspaceAddr,
+					KeyringAddr:    value.KeyringAddr,
+					Type:           value.Type.String(),
+					PublicKey:      value.PublicKey,
+					Index:          value.Index,
+					SignPolicyId:   value.SignPolicyId,
+					ZenbtcMetadata: value.ZenbtcMetadata,
+				},
+				Wallets: processWallets(value, types.WalletType_WALLET_TYPE_UNSPECIFIED, nil),
+			}, nil
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryZenbtcWalletsResponse{
+		ZenbtcWallets: keys,
+		Pagination:    pageRes,
+	}, nil
+}
---
+++ x/treasury/keeper/query_zenbtc_wallets_test.go
@@ -0,0 +1,369 @@
+package keeper_test
+
+import (
+	"testing"
+
+	keepertest "github.com/Zenrock-Foundation/zrchain/v5/testutil/keeper"
+	treasury "github.com/Zenrock-Foundation/zrchain/v5/x/treasury/module"
+	"github.com/Zenrock-Foundation/zrchain/v5/x/treasury/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/stretchr/testify/require"
+)
+
+var defaultECDSAKeyWithZenBTCMetadata = types.Key{
+	Id:            1,
+	WorkspaceAddr: "testWorkspace",
+	KeyringAddr:   "keyring1pfnq7r04rept47gaf5cpdew2",
+	Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+	PublicKey:     []byte{0x03, 0xca, 0x27, 0xea, 0x7b, 0x06, 0x41, 0x49, 0x7b, 0x19, 0xa7, 0x23, 0xe3, 0xb9, 0x25, 0x90, 0x80, 0x1c, 0x7e, 0x79, 0xb1, 0x14, 0x25, 0x3f, 0xc1, 0xe9, 0x9d, 0xf1, 0xfd, 0x97, 0x30, 0x52, 0x6b},
+	ZenbtcMetadata: &types.ZenBTCMetadata{
+		RecipientAddr: "0x9D450478FDB879C2900Ad54A0A407B0607b20478",
+		ChainType:     types.WalletType_WALLET_TYPE_EVM,
+		ChainId:       uint64(17000),
+		ReturnAddress: "tb1qypwjx7yj5jz0gw0vh76348ypa2ns7tfwsnhlh9",
+	},
+}
+
+var defaultECDSAKeyResponseWithZenBTCMetadata = types.KeyResponse{
+	Id:            1,
+	WorkspaceAddr: "testWorkspace",
+	KeyringAddr:   "keyring1pfnq7r04rept47gaf5cpdew2",
+	Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1.String(),
+	PublicKey:     []byte{0x03, 0xca, 0x27, 0xea, 0x7b, 0x06, 0x41, 0x49, 0x7b, 0x19, 0xa7, 0x23, 0xe3, 0xb9, 0x25, 0x90, 0x80, 0x1c, 0x7e, 0x79, 0xb1, 0x14, 0x25, 0x3f, 0xc1, 0xe9, 0x9d, 0xf1, 0xfd, 0x97, 0x30, 0x52, 0x6b},
+	ZenbtcMetadata: &types.ZenBTCMetadata{
+		RecipientAddr: "0x9D450478FDB879C2900Ad54A0A407B0607b20478",
+		ChainType:     types.WalletType_WALLET_TYPE_EVM,
+		ChainId:       uint64(17000),
+		ReturnAddress: "tb1qypwjx7yj5jz0gw0vh76348ypa2ns7tfwsnhlh9",
+	},
+}
+
+func TestKeeper_ZenBTCMetadata(t *testing.T) {
+	type args struct {
+		keys []types.Key
+		req  *types.QueryZenbtcWalletsRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *types.QueryZenbtcWalletsResponse
+		wantErr bool
+	}{
+		{
+			name: "PASS: return zenbtc metadata for empty request",
+			args: args{
+				keys: []types.Key{defaultECDSAKeyWithZenBTCMetadata},
+				req:  &types.QueryZenbtcWalletsRequest{},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: []*types.KeyAndWalletResponse{
+					{
+						Key: &defaultECDSAKeyResponseWithZenBTCMetadata,
+						Wallets: []*types.WalletResponse{
+							{Address: "zen1tun7x3s2ywksa32nl38d3fuuv8nk5ang97v73r", Type: types.WalletType_WALLET_TYPE_NATIVE.String()},
+							{Address: "0xdEa33aE3DA8f2EbA6efBB3EF5d143415438a6541", Type: types.WalletType_WALLET_TYPE_EVM.String()},
+						},
+					},
+				},
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   1,
+				},
+			},
+		},
+		{
+			name: "PASS: return zenbtc metadata for a recipient address request",
+			args: args{
+				keys: []types.Key{defaultECDSAKeyWithZenBTCMetadata},
+				req: &types.QueryZenbtcWalletsRequest{
+					RecipientAddr: "0x9D450478FDB879C2900Ad54A0A407B0607b20478",
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: []*types.KeyAndWalletResponse{
+					{
+						Key: &defaultECDSAKeyResponseWithZenBTCMetadata,
+						Wallets: []*types.WalletResponse{
+							{Address: "zen1tun7x3s2ywksa32nl38d3fuuv8nk5ang97v73r", Type: types.WalletType_WALLET_TYPE_NATIVE.String()},
+							{Address: "0xdEa33aE3DA8f2EbA6efBB3EF5d143415438a6541", Type: types.WalletType_WALLET_TYPE_EVM.String()},
+						},
+					},
+				},
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   1,
+				},
+			},
+		},
+		{
+			name: "PASS: return zenbtc metadata for a chain type request",
+			args: args{
+				keys: []types.Key{defaultECDSAKeyWithZenBTCMetadata},
+				req: &types.QueryZenbtcWalletsRequest{
+					ChainType: types.WalletType_WALLET_TYPE_EVM,
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: []*types.KeyAndWalletResponse{
+					{
+						Key: &defaultECDSAKeyResponseWithZenBTCMetadata,
+						Wallets: []*types.WalletResponse{
+							{Address: "zen1tun7x3s2ywksa32nl38d3fuuv8nk5ang97v73r", Type: types.WalletType_WALLET_TYPE_NATIVE.String()},
+							{Address: "0xdEa33aE3DA8f2EbA6efBB3EF5d143415438a6541", Type: types.WalletType_WALLET_TYPE_EVM.String()},
+						},
+					},
+				},
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   1,
+				},
+			},
+		},
+		{
+			name: "PASS: request only for EVM chain type",
+			args: args{
+				keys: []types.Key{
+					{
+						Id:            1,
+						WorkspaceAddr: "testWorkspace",
+						KeyringAddr:   "keyring1pfnq7r04rept47gaf5cpdew2",
+						Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+						PublicKey:     []byte{0x03, 0xca, 0x27, 0xea, 0x7b, 0x06, 0x41, 0x49, 0x7b, 0x19, 0xa7, 0x23, 0xe3, 0xb9, 0x25, 0x90, 0x80, 0x1c, 0x7e, 0x79, 0xb1, 0x14, 0x25, 0x3f, 0xc1, 0xe9, 0x9d, 0xf1, 0xfd, 0x97, 0x30, 0x52, 0x6b},
+						ZenbtcMetadata: &types.ZenBTCMetadata{
+							RecipientAddr: "dummySolAddress",
+							ChainType:     types.WalletType_WALLET_TYPE_SOLANA,
+							ChainId:       uint64(17000),
+							ReturnAddress: "tb1qypwjx7yj5jz0gw0vh76348ypa2ns7tfwsnhlh9",
+						},
+					},
+					defaultECDSAKeyWithZenBTCMetadata,
+				},
+				req: &types.QueryZenbtcWalletsRequest{
+					ChainType: types.WalletType_WALLET_TYPE_EVM,
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: []*types.KeyAndWalletResponse{
+					{
+						Key: &defaultECDSAKeyResponseWithZenBTCMetadata,
+						Wallets: []*types.WalletResponse{
+							{Address: "zen1tun7x3s2ywksa32nl38d3fuuv8nk5ang97v73r", Type: types.WalletType_WALLET_TYPE_NATIVE.String()},
+							{Address: "0xdEa33aE3DA8f2EbA6efBB3EF5d143415438a6541", Type: types.WalletType_WALLET_TYPE_EVM.String()},
+						},
+					},
+				},
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   1,
+				},
+			},
+		},
+		{
+			name: "PASS: return two keys with zenbtc metadata",
+			args: args{
+				keys: []types.Key{
+					{
+						Id:            2,
+						WorkspaceAddr: "testWorkspace",
+						KeyringAddr:   "keyring1pfnq7r04rept47gaf5cpdew2",
+						Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+						PublicKey:     []byte{0x03, 0xca, 0x27, 0xea, 0x7b, 0x06, 0x41, 0x49, 0x7b, 0x19, 0xa7, 0x23, 0xe3, 0xb9, 0x25, 0x90, 0x80, 0x1c, 0x7e, 0x79, 0xb1, 0x14, 0x25, 0x3f, 0xc1, 0xe9, 0x9d, 0xf1, 0xfd, 0x97, 0x30, 0x52, 0x6b},
+						ZenbtcMetadata: &types.ZenBTCMetadata{
+							RecipientAddr: "anotherETHAddress",
+							ChainType:     types.WalletType_WALLET_TYPE_EVM,
+							ChainId:       uint64(17000),
+							ReturnAddress: "tb1qypwjx7yj5jz0gw0vh76348ypa2ns7tfwsnhlh9",
+						},
+					},
+					defaultECDSAKeyWithZenBTCMetadata,
+				},
+				req: &types.QueryZenbtcWalletsRequest{
+					ChainType: types.WalletType_WALLET_TYPE_EVM,
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: []*types.KeyAndWalletResponse{
+					{
+						Key: &defaultECDSAKeyResponseWithZenBTCMetadata,
+						Wallets: []*types.WalletResponse{
+							{Address: "zen1tun7x3s2ywksa32nl38d3fuuv8nk5ang97v73r", Type: types.WalletType_WALLET_TYPE_NATIVE.String()},
+							{Address: "0xdEa33aE3DA8f2EbA6efBB3EF5d143415438a6541", Type: types.WalletType_WALLET_TYPE_EVM.String()},
+						},
+					},
+					{
+						Key: &types.KeyResponse{
+							Id:            2,
+							WorkspaceAddr: "testWorkspace",
+							KeyringAddr:   "keyring1pfnq7r04rept47gaf5cpdew2",
+							Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1.String(),
+							PublicKey:     []byte{0x03, 0xca, 0x27, 0xea, 0x7b, 0x06, 0x41, 0x49, 0x7b, 0x19, 0xa7, 0x23, 0xe3, 0xb9, 0x25, 0x90, 0x80, 0x1c, 0x7e, 0x79, 0xb1, 0x14, 0x25, 0x3f, 0xc1, 0xe9, 0x9d, 0xf1, 0xfd, 0x97, 0x30, 0x52, 0x6b},
+							ZenbtcMetadata: &types.ZenBTCMetadata{
+								RecipientAddr: "anotherETHAddress",
+								ChainType:     types.WalletType_WALLET_TYPE_EVM,
+								ChainId:       uint64(17000),
+								ReturnAddress: "tb1qypwjx7yj5jz0gw0vh76348ypa2ns7tfwsnhlh9",
+							},
+						},
+						Wallets: []*types.WalletResponse{
+							{Address: "zen1tun7x3s2ywksa32nl38d3fuuv8nk5ang97v73r", Type: types.WalletType_WALLET_TYPE_NATIVE.String()},
+							{Address: "0xdEa33aE3DA8f2EbA6efBB3EF5d143415438a6541", Type: types.WalletType_WALLET_TYPE_EVM.String()},
+						},
+					},
+				},
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   2,
+				},
+			},
+		},
+		{
+			name: "FAIL: request is nil",
+			args: args{
+				keys: []types.Key{defaultECDSAKey},
+				req:  nil,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "FAIL: ChainId does not match",
+			args: args{
+				keys: []types.Key{
+					{
+						Id:            1,
+						WorkspaceAddr: "testWorkspace1",
+						KeyringAddr:   "keyring1",
+						Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+						ZenbtcMetadata: &types.ZenBTCMetadata{
+							RecipientAddr: "recipient1",
+							ChainType:     types.WalletType_WALLET_TYPE_EVM,
+							ChainId:       uint64(17001),
+							ReturnAddress: "return1",
+						},
+					},
+				},
+				req: &types.QueryZenbtcWalletsRequest{
+					ChainId:   17000,
+					ChainType: types.WalletType_WALLET_TYPE_EVM,
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: nil,
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   0,
+				},
+			},
+		},
+		{
+			name: "FAIL: ChainType does not match",
+			args: args{
+				keys: []types.Key{
+					{
+						Id:            2,
+						WorkspaceAddr: "testWorkspace2",
+						KeyringAddr:   "keyring2",
+						Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+						ZenbtcMetadata: &types.ZenBTCMetadata{
+							RecipientAddr: "recipient2",
+							ChainType:     types.WalletType_WALLET_TYPE_BTC_REGNET,
+							ChainId:       uint64(17000),
+							ReturnAddress: "return2",
+						},
+					},
+				},
+				req: &types.QueryZenbtcWalletsRequest{
+					ChainType: types.WalletType_WALLET_TYPE_EVM,
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: nil,
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   0,
+				},
+			},
+		},
+		{
+			name: "FAIL: RecipientAddr does not match",
+			args: args{
+				keys: []types.Key{
+					{
+						Id:            3,
+						WorkspaceAddr: "testWorkspace3",
+						KeyringAddr:   "keyring3",
+						Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+						ZenbtcMetadata: &types.ZenBTCMetadata{
+							RecipientAddr: "differentRecipient",
+							ChainType:     types.WalletType_WALLET_TYPE_EVM,
+							ChainId:       uint64(17000),
+							ReturnAddress: "return3",
+						},
+					},
+				},
+				req: &types.QueryZenbtcWalletsRequest{
+					RecipientAddr: "recipient2",
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: nil,
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   0,
+				},
+			},
+		},
+		{
+			name: "FAIL: ReturnAddr does not match",
+			args: args{
+				keys: []types.Key{
+					{
+						Id:            4,
+						WorkspaceAddr: "testWorkspace4",
+						KeyringAddr:   "keyring4",
+						Type:          types.KeyType_KEY_TYPE_ECDSA_SECP256K1,
+						ZenbtcMetadata: &types.ZenBTCMetadata{
+							RecipientAddr: "recipient4",
+							ChainType:     types.WalletType_WALLET_TYPE_EVM,
+							ChainId:       uint64(17000),
+							ReturnAddress: "differentReturn",
+						},
+					},
+				},
+				req: &types.QueryZenbtcWalletsRequest{
+					ReturnAddr: "return3",
+				},
+			},
+			want: &types.QueryZenbtcWalletsResponse{
+				ZenbtcWallets: nil,
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   0,
+				},
+			},
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			keepers := keepertest.NewTest(t)
+			tk := keepers.TreasuryKeeper
+			ctx := keepers.Ctx
+
+			genesis := types.GenesisState{
+				PortId: types.PortID,
+				Keys:   tt.args.keys,
+			}
+			treasury.InitGenesis(ctx, *tk, genesis)
+
+			got, err := tk.ZenbtcWallets(ctx, tt.args.req)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
