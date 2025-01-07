4edd0c52e7574a2cd3038d78ffacfc24edc410f0
Sasha Duke <sasha@zenrocklabs.io>
2025-01-07 13:25:14 +0000

    fix: query unstaked redemptions (#102)

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,11 +4,11 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades"
-	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades/v5beta5"
+	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades/v5beta6"
 )
 
 var Upgrades = []upgrades.Upgrade{
-	v5beta5.Upgrade,
+	v5beta6.Upgrade,
 }
 
 func (app ZenrockApp) RegisterUpgradeHandlers() {
