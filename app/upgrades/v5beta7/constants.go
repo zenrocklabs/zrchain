c84086e56e74539544c1be1e3bdb62135fa3e970
Sasha Duke <sasha@zenrocklabs.io>
2025-01-08 08:53:45 +0000

    fix: add `v5beta7` upgrade files (#104)

---
+++ app/upgrades/v5beta7/constants.go
@@ -0,0 +1,18 @@
+package v5beta7
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades"
+)
+
+const UpgradeName = "v5beta7"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+		Renamed: []storetypes.StoreRename{},
+	},
+}
