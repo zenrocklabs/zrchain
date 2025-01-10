1183ce9d0468e2f1dc11c991420019c7826c3954
Sasha Duke <sasha@zenrocklabs.io>
2025-01-10 19:48:51 +0000

    fix: rename upgrade to `v5rev3`

---
+++ app/upgrades/v5rev3/constants.go
@@ -0,0 +1,18 @@
+package v5rev3
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades"
+)
+
+const UpgradeName = "v5rev3"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+		Renamed: []storetypes.StoreRename{},
+	},
+}
