2e9e919c3777c9862ff25fe2f1d005cb03592107
Sasha Duke <sasha@zenrocklabs.io>
2025-01-16 03:25:57 +0000

    add upgrade files

---
+++ app/upgrades/v5temp5/constants.go
@@ -0,0 +1,18 @@
+package v5temp5
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/Zenrock-Foundation/zrchain/v5/app/upgrades"
+)
+
+const UpgradeName = "v5temp5"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+		Renamed: []storetypes.StoreRename{},
+	},
+}
