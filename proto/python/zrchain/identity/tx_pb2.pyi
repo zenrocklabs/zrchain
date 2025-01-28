00943f021e816ea01935f013e979b20c15791aff
Ognyan Nikolov <ognyan@zenrocklabs.io>
2025-01-28 16:22:45 +0200

    regenerate proto after main merge

--- proto/python/zrchain/identity/tx_pb2.pyi
+++ proto/python/zrchain/identity/tx_pb2.pyi
@@ -105,24 +105,20 @@ class MsgRemoveWorkspaceOwnerResponse(_message.Message):
     def __init__(self) -> None: ...
 
 class MsgNewKeyring(_message.Message):
-    __slots__ = ("creator", "description", "party_threshold", "key_req_fee", "sig_req_fee", "delegate_fees", "mpc_minimum_timeout", "mpc_default_timeout")
+    __slots__ = ("creator", "description", "party_threshold", "key_req_fee", "sig_req_fee", "delegate_fees")
     CREATOR_FIELD_NUMBER: _ClassVar[int]
     DESCRIPTION_FIELD_NUMBER: _ClassVar[int]
     PARTY_THRESHOLD_FIELD_NUMBER: _ClassVar[int]
     KEY_REQ_FEE_FIELD_NUMBER: _ClassVar[int]
     SIG_REQ_FEE_FIELD_NUMBER: _ClassVar[int]
     DELEGATE_FEES_FIELD_NUMBER: _ClassVar[int]
-    MPC_MINIMUM_TIMEOUT_FIELD_NUMBER: _ClassVar[int]
-    MPC_DEFAULT_TIMEOUT_FIELD_NUMBER: _ClassVar[int]
     creator: str
     description: str
     party_threshold: int
     key_req_fee: int
     sig_req_fee: int
     delegate_fees: bool
-    mpc_minimum_timeout: int
-    mpc_default_timeout: int
-    def __init__(self, creator: _Optional[str] = ..., description: _Optional[str] = ..., party_threshold: _Optional[int] = ..., key_req_fee: _Optional[int] = ..., sig_req_fee: _Optional[int] = ..., delegate_fees: bool = ..., mpc_minimum_timeout: _Optional[int] = ..., mpc_default_timeout: _Optional[int] = ...) -> None: ...
+    def __init__(self, creator: _Optional[str] = ..., description: _Optional[str] = ..., party_threshold: _Optional[int] = ..., key_req_fee: _Optional[int] = ..., sig_req_fee: _Optional[int] = ..., delegate_fees: bool = ...) -> None: ...
 
 class MsgNewKeyringResponse(_message.Message):
     __slots__ = ("addr",)
@@ -147,7 +143,7 @@ class MsgAddKeyringPartyResponse(_message.Message):
     def __init__(self) -> None: ...
 
 class MsgUpdateKeyring(_message.Message):
-    __slots__ = ("creator", "keyring_addr", "party_threshold", "key_req_fee", "sig_req_fee", "description", "is_active", "mpc_minimum_btl", "mpc_default_btl")
+    __slots__ = ("creator", "keyring_addr", "party_threshold", "key_req_fee", "sig_req_fee", "description", "is_active")
     CREATOR_FIELD_NUMBER: _ClassVar[int]
     KEYRING_ADDR_FIELD_NUMBER: _ClassVar[int]
     PARTY_THRESHOLD_FIELD_NUMBER: _ClassVar[int]
@@ -155,8 +151,6 @@ class MsgUpdateKeyring(_message.Message):
     SIG_REQ_FEE_FIELD_NUMBER: _ClassVar[int]
     DESCRIPTION_FIELD_NUMBER: _ClassVar[int]
     IS_ACTIVE_FIELD_NUMBER: _ClassVar[int]
-    MPC_MINIMUM_BTL_FIELD_NUMBER: _ClassVar[int]
-    MPC_DEFAULT_BTL_FIELD_NUMBER: _ClassVar[int]
     creator: str
     keyring_addr: str
     party_threshold: int
@@ -164,9 +158,7 @@ class MsgUpdateKeyring(_message.Message):
     sig_req_fee: int
     description: str
     is_active: bool
-    mpc_minimum_btl: int
-    mpc_default_btl: int
-    def __init__(self, creator: _Optional[str] = ..., keyring_addr: _Optional[str] = ..., party_threshold: _Optional[int] = ..., key_req_fee: _Optional[int] = ..., sig_req_fee: _Optional[int] = ..., description: _Optional[str] = ..., is_active: bool = ..., mpc_minimum_btl: _Optional[int] = ..., mpc_default_btl: _Optional[int] = ...) -> None: ...
+    def __init__(self, creator: _Optional[str] = ..., keyring_addr: _Optional[str] = ..., party_threshold: _Optional[int] = ..., key_req_fee: _Optional[int] = ..., sig_req_fee: _Optional[int] = ..., description: _Optional[str] = ..., is_active: bool = ...) -> None: ...
 
 class MsgUpdateKeyringResponse(_message.Message):
     __slots__ = ()
