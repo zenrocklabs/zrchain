597e561d5445d62923744a9f945194e10fe1c812
Martin <martin@zenrocklabs.io>
2025-02-03 16:12:26 +0100

    fix: treasury migration script

---
+++ x/treasury/migrations/v4/store.go
@@ -0,0 +1,26 @@
+package v4
+
+import (
+	"cosmossdk.io/collections"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/Zenrock-Foundation/zrchain/v5/x/treasury/types"
+)
+
+func RejectBadTestnetRequests(ctx sdk.Context, signRequestStore collections.Map[uint64, types.SignRequest], codec codec.BinaryCodec) error {
+	if err := signRequestStore.Walk(ctx, nil, func(id uint64, signReq types.SignRequest) (bool, error) {
+		if len(signReq.DataForSigning) == 0 || len(signReq.DataForSigning[0]) == 0 {
+			signReq.Status = types.SignRequestStatus_SIGN_REQUEST_STATUS_REJECTED
+			signReq.RejectReason = "data for signing is empty"
+			if err := signRequestStore.Set(ctx, id, signReq); err != nil {
+				return true, err
+			}
+		}
+		return false, nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
