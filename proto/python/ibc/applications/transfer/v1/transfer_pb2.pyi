4fa9824a0e01473a6084171d6f3ac209fbe61d2e
Sasha Duke <sasha@zenrocklabs.io>
2025-02-18 15:58:42 +0000

    fix: add publicKey field to keyRequest for validation

--- proto/python/ibc/applications/transfer/v1/transfer_pb2.pyi
+++ proto/python/ibc/applications/transfer/v1/transfer_pb2.pyi
@@ -1,8 +1,7 @@
 from gogoproto import gogo_pb2 as _gogo_pb2
-from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -13,14 +12,6 @@ class Params(_message.Message):
     send_enabled: bool
     receive_enabled: bool
     def __init__(self, send_enabled: bool = ..., receive_enabled: bool = ...) -> None: ...
-
-class Forwarding(_message.Message):
-    __slots__ = ("unwind", "hops")
-    UNWIND_FIELD_NUMBER: _ClassVar[int]
-    HOPS_FIELD_NUMBER: _ClassVar[int]
-    unwind: bool
-    hops: _containers.RepeatedCompositeFieldContainer[Hop]
-    def __init__(self, unwind: bool = ..., hops: _Optional[_Iterable[_Union[Hop, _Mapping]]] = ...) -> None: ...
 
 class Hop(_message.Message):
     __slots__ = ("port_id", "channel_id")
