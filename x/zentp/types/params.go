472e3a7820e21b4b91fc3de04a251d82b5fc7518
Sasha Duke <sasha@zenrocklabs.io>
2025-04-23 15:11:43 +0100

    misc fixes

--- x/zentp/types/params.go
+++ x/zentp/types/params.go
@@ -5,15 +5,15 @@ import (
 )
 
 var DefaultSolanaParams = &Solana{
-	SignerKeyId:       7,
+	SignerKeyId:       10,
 	ProgramId:         "3jo4mdc6QbGRigia2jvmKShbmz3aWq4Y8bgUXfur5StT",
-	NonceAuthorityKey: 8,
-	NonceAccountKey:   9,
+	NonceAuthorityKey: 11,
+	NonceAccountKey:   12,
 	MintAddress:       "9oBkgQUkq8jvzK98D7Uib6GYSZZmjnZ6QEGJRrAeKnDj",
 	FeeWallet:         "FzqGcRG98v1KhKxatX2Abb2z1aJ2rViQwBK5GHByKCAd",
 	Fee:               0,
+	Btl:               20,
 	// MultisigKeyAddress: "8cmZY2id22vxpXs2H3YYQNARuPHNuYwa7jipW1q1v9Fy",
-	Btl: 20,
 }
 
 var _ paramtypes.ParamSet = (*Params)(nil)
