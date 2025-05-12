d6ede3857dd867d98be162067c67a1da3fdc40bb
Sasha Duke <sasha@zenrocklabs.io>
2025-05-12 13:28:45 +0100

    fix: panic in `new-signature-request` handler (#268)

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,11 +4,11 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades"
-	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades/v6rev1"
+	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades/v6rev4"
 )
 
 var Upgrades = []upgrades.Upgrade{
-	v6rev1.Upgrade,
+	v6rev4.Upgrade,
 }
 
 func (app ZenrockApp) RegisterUpgradeHandlers() {
