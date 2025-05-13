743f05272c83eff62a9fa7ed0322129a812cbe16
Sasha Duke <sasha@zenrocklabs.io>
2025-05-13 17:11:58 +0100

    fix: rockBTC stake power

--- contracts/solrock/durablenonce_test.go
+++ contracts/solrock/durablenonce_test.go
@@ -16,8 +16,8 @@ func TestCreateDurableNonceAccount(t *testing.T) {
 
 	var client = rpc.New("https://api.devnet.solana.com")
 
-	nonceAuthPubKey := solana.MustPublicKeyFromBase58("9RmRUp1P5Y4ZoSVtXsNtW5oZdxQdfLe81ejcAu3bRXsc")
-	nonceAccPubKey := solana.MustPublicKeyFromBase58("2f7kpH1H4RNYE2SVJV5J16u68rqepSQkFmMUdFeHXsMR")
+	nonceAuthPubKey := solana.MustPublicKeyFromBase58("HJUmKc6Mb1CvuM2cFSjz1YthEFUjsRMCveBT9Vh5GsEr")
+	nonceAccPubKey := solana.MustPublicKeyFromBase58("Dst4xMLREPRixxtxz435NF4W1DEBMr4JUd7Wxwv39FHg")
 	recent, err := client.GetLatestBlockhash(context.Background(), rpc.CommitmentConfirmed)
 	require.NoError(t, err)
 	require.NoError(t, err)
@@ -45,7 +45,7 @@ func TestCreateDurableNonceAccount(t *testing.T) {
 	bin, err := tx.Message.MarshalBinary()
 	require.NoError(t, err)
 
-	fmt.Printf("transaction : %s", hex.EncodeToString(bin))
+	fmt.Printf("transaction hex data: %s", hex.EncodeToString(bin))
 }
 
 func TestParseDurableNonceTransaction(t *testing.T) {
