7ffffb405dae60937b5654ce43fd483d31a7528b
Martin <martin@zenrocklabs.io>
2025-05-20 15:36:50 +0200

    add github runner logs

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -7,6 +7,6 @@ rm -rf ~/.zrchain
 
 # Run the integration tests with verbose output
 echo "Running integration tests..."
-go test -v -count=1 -tags "integration,!wasm" ./tests/integration/keyring_test.go
 go test -v -count=1 -tags "integration,!wasm" ./tests/integration/action_test.go
 go test -v -count=1 -tags "integration,!wasm" ./tests/integration/mpc_ecdsa_keygen_test.go
+
