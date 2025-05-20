9291ce0587f6cb934c5f45fbb6f06f36b2e5303d
Martin <martin@zenrocklabs.io>
2025-05-20 13:12:54 +0200

    exclude wasm from integration test

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env sh
 set -ex  # Add -x for verbose output
 
-# Print environment information
-echo "Go version:"
-go version
-
-echo "Go environment:"
-go env
-
 # Clean up previous config
 echo "Cleaning up previous config..."
 rm -rf ~/.zrchain
 
 # Run the integration tests with verbose output
 echo "Running integration tests..."
-go test -v -count=1 ./tests/integration/mpc_ecdsa_keygen_test.go
+go test -v -count=1 -tags "integration,!wasm" ./tests/integration/keyring_test.go
+go test -v -count=1 -tags "integration,!wasm" ./tests/integration/action_test.go
+go test -v -count=1 -tags "integration,!wasm" ./tests/integration/mpc_ecdsa_keygen_test.go
