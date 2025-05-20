0749473fc817084c707847aa03743ba9ec94e5a3
Martin <martin@zenrocklabs.io>
2025-05-20 12:50:48 +0200

    add verbose logging, and run without caching

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env sh
-set -e
+set -ex  # Add -x for verbose output
+
+# Print environment information
+echo "Go version:"
+go version
+
+echo "Go environment:"
+go env
 
 # Clean up previous config
+echo "Cleaning up previous config..."
 rm -rf ~/.zrchain
 
-# Run the integration tests
-go test -v ./tests/integration/mpc_ecdsa_keygen_test.go -tags=integration
+# Run the integration tests with verbose output
+echo "Running integration tests..."
+go test -v -count=1 ./tests/integration/mpc_ecdsa_keygen_test.go
