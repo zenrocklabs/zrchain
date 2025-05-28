44ee43efcdde4960fb1d1e60882077352b24d7ea
Martin <martin@zenrocklabs.io>
2025-05-28 13:15:11 +0200

    fix: revert sol redeem event

--- x/validation/keeper/abci.go
+++ x/validation/keeper/abci.go
@@ -353,9 +353,9 @@ func (k *Keeper) PreBlocker(ctx sdk.Context, req *abci.RequestFinalizeBlock) err
 		if fieldHasConsensus(oracleData.FieldVotePowers, VEFieldEthBurnEventsHash) {
 			k.storeNewZenBTCBurnEventsEthereum(ctx, oracleData)
 		}
-		if fieldHasConsensus(oracleData.FieldVotePowers, VEFieldSolanaBurnEventsHash) {
-			k.storeNewZenBTCBurnEventsSolana(ctx, oracleData)
-		}
+		// if fieldHasConsensus(oracleData.FieldVotePowers, VEFieldSolanaBurnEventsHash) {
+		// 	k.storeNewZenBTCBurnEventsSolana(ctx, oracleData)
+		// }
 		if fieldHasConsensus(oracleData.FieldVotePowers, VEFieldRedemptionsHash) {
 			k.storeNewZenBTCRedemptions(ctx, oracleData)
 		}
@@ -364,7 +364,7 @@ func (k *Keeper) PreBlocker(ctx sdk.Context, req *abci.RequestFinalizeBlock) err
 		k.processZenBTCMintsEthereum(ctx, oracleData)
 		k.processZenBTCMintsSolana(ctx, oracleData)
 		k.processSolanaZenBTCMintEvents(ctx, oracleData)
-		k.processZenBTCBurnEvents(ctx, oracleData)
+		// k.processZenBTCBurnEvents(ctx, oracleData)
 		k.processZenBTCRedemptions(ctx, oracleData)
 		k.checkForRedemptionFulfilment(ctx)
 		k.processSolanaROCKMints(ctx, oracleData)
