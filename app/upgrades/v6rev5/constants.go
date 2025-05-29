d1736b88fd0a0aa6d1392cf024d6bb8923b17ef8
Sasha Duke <sasha@zenrocklabs.io>
2025-05-29 12:57:25 +0100

    fix: cleanup devnet state (#283)

---
+++ app/upgrades/v6rev5/constants.go
@@ -0,0 +1,18 @@
+package v6rev5
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades"
+)
+
+const UpgradeName = "v6rev5"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+		Renamed: []storetypes.StoreRename{},
+	},
+}
