408c1b1b0fb96cc8756e4e7a7f9abaa153c8e57c
Sasha Duke <sasha@zenrocklabs.io>
2025-06-16 19:00:58 +0100

    wip

---
+++ go-client/query_client_zentp.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"context"
+
+	"github.com/Zenrock-Foundation/zrchain/v6/x/zentp/types"
+	"google.golang.org/grpc"
+)
+
+type ZenTPQueryClient struct {
+	client types.QueryClient
+}
+
+func NewZenTPQueryClient(c *grpc.ClientConn) *ZenTPQueryClient {
+	return &ZenTPQueryClient{
+		client: types.NewQueryClient(c),
+	}
+}
+
+func (c *ZenTPQueryClient) Params(ctx context.Context) (*types.QueryParamsResponse, error) {
+	return c.client.Params(ctx, &types.QueryParamsRequest{})
+}
+
+func (c *ZenTPQueryClient) Burns(ctx context.Context, startIndex uint64, txID string, logIndex uint64, chainID string) (*types.QueryBurnsResponse, error) {
+	return c.client.Burns(ctx, &types.QueryBurnsRequest{})
+}
