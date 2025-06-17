84e65d3098f7731253d567bf6861489df20accb3
Sasha Duke <sasha@zenrocklabs.io>
2025-06-17 16:24:46 +0100

    TriggerEventBackfill handler

--- x/validation/keeper/msg_server_trigger_event_backfill.go
+++ x/validation/keeper/msg_server_trigger_event_backfill.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	"github.com/Zenrock-Foundation/zrchain/v6/x/validation/types"
 )
@@ -10,6 +11,21 @@ import (
 func (k msgServer) TriggerEventBackfill(ctx context.Context, msg *types.MsgTriggerEventBackfill) (*types.MsgTriggerEventBackfillResponse, error) {
 	if k.authority != msg.Authority {
 		return nil, fmt.Errorf("invalid authority; expected %s, got %s", k.authority, msg.Authority)
+	}
+
+	backfillRequests, err := k.BackfillRequests.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if slices.Contains(backfillRequests.Requests, msg) {
+		return nil, fmt.Errorf("backfill request already exists")
+	}
+
+	backfillRequests.Requests = append(backfillRequests.Requests, msg)
+
+	if err = k.BackfillRequests.Set(ctx, backfillRequests); err != nil {
+		return nil, err
 	}
 
 	return &types.MsgTriggerEventBackfillResponse{}, nil
