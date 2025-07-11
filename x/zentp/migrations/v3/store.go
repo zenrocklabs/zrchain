c6b8663b350870d71054cb29a55fc51ef9140875
Martin <martin@zenrocklabs.io>
2025-07-11 11:06:11 +0200

    fix: zentp v3 migration

--- x/zentp/migrations/v3/store.go
+++ x/zentp/migrations/v3/store.go
@@ -1,7 +1,10 @@
 package v3
 
 import (
+	"context"
+
 	"cosmossdk.io/collections"
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/Zenrock-Foundation/zrchain/v6/x/zentp/types"
@@ -38,6 +41,47 @@ func UpdateBurnStore(ctx sdk.Context, oldBurnsCol collections.Map[uint64, types.
 
 	for _, burn := range burns {
 		if err := newBurnsCol.Set(ctx, burn.Id, burn); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func SendZentpFeesToMintModule(
+	ctx sdk.Context,
+	getPendingMints func(context.Context) ([]*types.Bridge, error),
+	getBridgeFeeParams func(context.Context) (sdk.AccAddress, math.LegacyDec, error),
+	bankKeeper types.BankKeeper,
+	accountKeeper types.AccountKeeper,
+) error {
+	pendingMints, err := getPendingMints(ctx)
+	if err != nil {
+		return err
+	}
+
+	var pendingFees sdk.Coins
+	_, bridgeFee, err := getBridgeFeeParams(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, mint := range pendingMints {
+		amountInt := math.NewIntFromUint64(mint.Amount)
+		bridgeFeeAmount := math.LegacyNewDecFromInt(amountInt).Mul(bridgeFee).TruncateInt()
+		if bridgeFeeAmount.IsPositive() {
+			pendingFees = pendingFees.Add(sdk.NewCoin(mint.Denom, bridgeFeeAmount))
+		}
+	}
+
+	zentpAddr := accountKeeper.GetModuleAddress(types.ModuleName)
+	zentpBalance := bankKeeper.SpendableCoins(ctx, zentpAddr)
+
+	amountToSend := zentpBalance.Sub(pendingFees...)
+
+	if amountToSend.IsAllPositive() {
+		err = bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, types.ZentpCollectorName, amountToSend)
+		if err != nil {
 			return err
 		}
 	}
