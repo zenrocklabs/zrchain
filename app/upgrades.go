d52f2afb863dca0b308324c70bf9f2e545b2e388
Sasha Duke <sasha@zenrocklabs.io>
2025-10-06 13:12:09 +0100

    upgrade files

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,11 +4,11 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades"
-	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades/v6rev31"
+	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades/v6rev31p1"
 )
 
 var Upgrades = []upgrades.Upgrade{
-	v6rev31.Upgrade,
+	v6rev31p1.Upgrade,
 }
 
 func (app ZenrockApp) RegisterUpgradeHandlers() {
