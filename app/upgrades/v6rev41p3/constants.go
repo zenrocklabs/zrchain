e9d3f725075fb8050246220ea857bd9073eb5473
Sasha Duke <sasha@zenrocklabs.io>
2025-10-17 00:20:47 +0100

    upgrade files

---
+++ app/upgrades/v6rev41p3/constants.go
@@ -0,0 +1,18 @@
+package v6rev41p3
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades"
+)
+
+const UpgradeName = "v6rev41p3"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+		Renamed: []storetypes.StoreRename{},
+	},
+}
