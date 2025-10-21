040546f824364c5ff185a0fdce50d63745ced225
Martin <martin@zenrocklabs.io>
2025-10-21 08:58:37 +0200

    feat: v6rev43 upgrade

---
+++ app/upgrades/v6rev43/constants.go
@@ -0,0 +1,18 @@
+package v6rev43
+
+import (
+	storetypes "cosmossdk.io/store/types"
+	"github.com/Zenrock-Foundation/zrchain/v6/app/upgrades"
+)
+
+const UpgradeName = "v6rev43"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+		Renamed: []storetypes.StoreRename{},
+	},
+}
